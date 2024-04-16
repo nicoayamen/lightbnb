6ed643588ec75d22b2808234ae74648c4602c79a
nicoayamen <nicolas.ayala4@gmail.com>
2024-04-16 17:39:19 -0600

    refactor getAllReservation to take sql query and retrun promise

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -76,7 +76,25 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(
+      `SELECT reservations.*, properties.*, avg(rating) as average_rating
+      FROM reservations
+      JOIN properties ON reservations.property_id = properties.id
+      LEFT JOIN property_reviews ON properties.id = property_reviews.property_id
+      WHERE guest_id = $1
+      GROUP BY reservations.id, properties.id
+      ORDER BY reservations.start_date
+      LIMIT $2`,
+      [guest_id, limit]
+    )
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.error('Error executing getAllReservations:', err.message);
+      throw err;
+    });
 };
 
 /// Properties
